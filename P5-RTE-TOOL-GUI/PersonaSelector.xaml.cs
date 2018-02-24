bf38f0384ce5ef9f1ef143d9e8a6ff7bdef3f0b8
Muhammad Ariq Srihadi Wibowoputra <muhammadariq13@gmail.com>
2018-02-25 00:10:09 +0700

    Add more functions to SkillSelector and PersonaSelector

--- P5-RTE-TOOL-GUI/PersonaSelector.xaml.cs
+++ P5-RTE-TOOL-GUI/PersonaSelector.xaml.cs
@@ -15,13 +15,66 @@ using System.Windows.Shapes;
 namespace P5_RTE_TOOL_GUI
 {
     /// <summary>
-    /// Interaction logic for Window1.xaml
+    /// Interaction logic for PersonaSelector.xaml
     /// </summary>
-    public partial class Window1 : Window
+    public partial class PersonaSelector : Window
     {
-        public Window1()
+
+        public string PersonaSelection = "XXXX";
+        public bool HasConfirmedSelection = false;
+        public List<object> TrueCollection = new List<object>();
+        public List<object> LastCollection = new List<object>();
+        public bool SearchChanged = false;
+
+        public PersonaSelector()
         {
             InitializeComponent();
+        }
+
+        private void PersonaSelector_Load(object sender, EventArgs e)
+        {
+            //Store the true personaList collection for later use
+            for (int i = 0; i < personaList.Items.Count; i++)
+                TrueCollection.Add(personaList.Items[i]);
+            LastCollection = TrueCollection;
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            string search = searchInput.Text;
+            List<object> curCollection = new List<object>();
+
+            if (search != "")
+            {
+                //Go through LastCollection and add potential searches to curCollection
+                foreach (object item in LastCollection)
+                {
+                    if (item.ToString().Contains(search, StringComparison.InvariantCultureIgnoreCase))
+                        curCollection.Add(item);
+                }
+            }
+            else
+                curCollection = TrueCollection;
+
+            //Clear personaList and fill it with curCollection
+            personaList.Items.Clear();
+            foreach (object item in curCollection)
+                personaList.Items.Add(item);
+            //Set LastCollection as curCollection
+            LastCollection = curCollection;
+        }
+
+        private void confirmButton_Click(object sender, EventArgs e)
+        {
+            if (personaList.SelectedItem != null)
+            {
+                PersonaSelection = personaList.SelectedItem.ToString();
+                PersonaSelection = PersonaSelection.Remove(0, PersonaSelection.Length - 4);
+                HasConfirmedSelection = true;
+                Close();
+            }
+            else
+                MessageBox.Show("Please select an entry from the list!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
--- P5-RTE-TOOL-GUI/SkillSelector.xaml.cs
+++ P5-RTE-TOOL-GUI/SkillSelector.xaml.cs
@@ -19,9 +19,61 @@ namespace P5_RTE_TOOL_GUI
     /// </summary>
     public partial class SkillSelector : Window
     {
+
+        public string SkillSelection = "";
+        public bool HasConfirmedSelection = false;
+        public List<object> TrueCollection = new List<object>();
+        public List<object> LastCollection = new List<object>();
+
         public SkillSelector()
         {
             InitializeComponent();
+        }
+
+        private void SkillSelector_Load(object sender, EventArgs e)
+        {
+            //Store the true skillList collection for later use
+            for (int i = 0; i < skillList.Items.Count; i++)
+                TrueCollection.Add(skillList.Items[i]);
+            LastCollection = TrueCollection;
+        }
+
+        private void confirmButton_Click(object sender, EventArgs e)
+        {
+            if (skillList.SelectedItem != null)
+            {
+                SkillSelection = skillList.SelectedItem.ToString();
+                SkillSelection = SkillSelection.Remove(0, SkillSelection.Length - 4);
+                HasConfirmedSelection = true;
+                Close();
+            }
+            else
+                MessageBox.Show("Please select an entry from the list!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            string search = searchInput.Text;
+            List<object> curCollection = new List<object>();
+
+            if (search != "")
+            {
+                //Go through LastCollection and add potential searches to curCollection
+                foreach (object item in LastCollection)
+                {
+                    if (item.ToString().Contains(search, StringComparison.InvariantCultureIgnoreCase))
+                        curCollection.Add(item);
+                }
+            }
+            else
+                curCollection = TrueCollection;
+
+            //Clear personaList and fill it with curCollection
+            skillList.Items.Clear();
+            foreach (object item in curCollection)
+                skillList.Items.Add(item);
+            //Set LastCollection as curCollection
+            LastCollection = curCollection;
         }
     }
 }
